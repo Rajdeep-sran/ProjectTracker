34743779227245aed48e26d1b8f32c7d6708dbb5
Rajdeep Kaur <rajdeepkaur11064bms@gmail.com>
2025-07-22 13:42:59 +0530

    Update settings.py

--- Project_Manager/settings.py
+++ Project_Manager/settings.py
@@ -130,9 +130,12 @@ LOGIN_URL = '/signin/'
 LOGIN_REDIRECT_URL = '/dashboard/'
 
 # Email settings (for password reset)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # Or your email provider
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'rajdeepkaur11064bms@gmail.com'       
-EMAIL_HOST_PASSWORD = 'sbkyhfrmoawnelad'     
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+
+  
